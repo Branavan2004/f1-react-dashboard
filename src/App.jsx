2b568e4c92c23c9eba30c09bb25f4c788e44c151
Branavan <branavan09@gmail.com>
2026-02-05 02:36:22 +0530

    First F1 Dashboard commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+
+import DriverCard from "./DriverCard"
+
+function App() {
+  return (
+    
+    <div style={{ textAlign: 'center',
+     marginTop: '500px', 
+     marginLeft:'500px',
+      marginRight: '500px' , 
+      textAlign: 'center'}}>
+      <h1>F1 Dashboard</h1>
+      <p>Welcome to the paddock, Branavan!</p>
+      <DriverCard/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/DriverCard.jsx
@@ -0,0 +1,17 @@
+function DriverCard() {
+  return (
+    <div style={{ 
+
+     border: '2px solid red', 
+      padding: '20px', 
+      width: '250px',
+      textAlign: 'center'
+
+    }}>
+      <h2>Max Verstappen</h2>
+      <p>Team: Red Bull</p>
+    </div>
+  )
+}
+
+export default DriverCard
